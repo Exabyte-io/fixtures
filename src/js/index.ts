94514c8a7b9b3a9b48fe1cf9ea84a5cd55b4bc8e
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-01-20 17:55:23 +0200

    feat: add input_templates_rendered and npm package configuration
    - Add input_templates_rendered folder (not in LFS)
    - Configure npm package to export data folder
    - Update README with JavaScript/TypeScript usage
    - Add data folder path exports

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,19 @@
-export function greet(name: string): string {
-    return `Hello ${name}!`;
+import * as path from "path";
+
+/**
+ * Get the path to the data folder
+ * @returns The absolute path to the data folder
+ */
+export function getDataPath(): string {
+    return path.join(__dirname, "../../data");
 }
+
+/**
+ * The path to the data folder
+ */
+export const DATA_PATH = path.join(__dirname, "../../data");
+
+/**
+ * Export the data folder path as the default export
+ */
+export default DATA_PATH;
