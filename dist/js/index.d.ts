ab4bcb91600fab20c940d19312f9c121178e0cee
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-01-20 18:12:45 +0200

    chore: dist

---
+++ dist/js/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Get the path to the data folder
+ * @returns The absolute path to the data folder
+ */
+export declare function getDataPath(): string;
+/**
+ * The path to the data folder
+ */
+export declare const DATA_PATH: string;
+/**
+ * Export the data folder path as the default export
+ */
+export default DATA_PATH;
